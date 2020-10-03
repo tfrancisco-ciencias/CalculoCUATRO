7107021816466cd7f87f20c38fa931eda0524b1f
Francisco <tfrancisco.math@gmail.com>
2020-10-03 13:59:03 -0500

    Added sections 3,4

--- main.js
+++ main.js
@@ -2,4 +2,7 @@ $(document).ready(function() {
   $("button.respButton").click(function() {
        $(this).next("div.resp").toggle();
   });
+  $("button.respButton").click(function() {
+       $(this).next("div.resp-box").toggle();
+  });
 });
